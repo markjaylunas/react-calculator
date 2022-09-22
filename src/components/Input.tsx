f33a7471bd55cd1468275d6ab90e0d8fdd7a5dc8
Mark Jay Lunas <you@exagit config --global user.email markjay.lunas@gmail.com git config --global user.email you@exa>
2022-09-22 11:11:29 +0800

    input in two liners

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,21 +1,8 @@
-import { useState } from 'react';
-
 const Input: React.FC = () => {
-    const [inputField, setInputField] = useState('');
-
-    const onChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
-        const value = e.target.value;
-        setInputField(value.replace(/\D/g, ''));
-    };
     return (
-        <div className="w-full h-full">
-            <input
-                type="tel"
-                name="inputField"
-                value={inputField}
-                onChange={onChange}
-                className="rounded-md text-3xl font-bold border-2 border-slate-800 px-5 py-2 focus:outline-none focus:ring-2 ring-slate-900 focus:border-slate-700  hover:border-slate-700   transition delay-75 ease-in-out w-full"
-            />
+        <div className="rounded-md flex flex-col items-end  w-full h-full border-2 border-slate-800 px-5 py-2  focus:ring-2 ring-slate-900 focus:border-slate-700  hover:border-slate-700   transition delay-75 ease-in-out ">
+            <span className="text-slate-400">234 - 44 =</span>
+            <span className="text-3xl font-bold">234354534</span>
         </div>
     );
 };
